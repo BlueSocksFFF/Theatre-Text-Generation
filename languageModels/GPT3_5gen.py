1758ae78bbf3285900453ff21e8bcd8845264f18
Kyle Jung <kyle12jung@gmail.com>
2023-03-17 14:56:37 -0500

    multiprocessing implemented

--- languageModels/GPT3_5gen.py
+++ languageModels/GPT3_5gen.py
@@ -18,13 +18,10 @@ class gpt3_5_monologue_generator:
       model="gpt-3.5-turbo",
       messages=[{"role": "user", "content": prompt}],
       temperature=0.8,
-      max_tokens=300,
+      max_tokens=300, 
       frequency_penalty=0.5,
       presence_penalty=0.0
     )["choices"][0]["message"]["content"]
-    # fo = open("generatedTexts/gpt3_generated_text.txt","a")
-    # fo.write("\nGPT3 from " + self.prompt + ": " + response)
-    # fo.close()
     with open("generatedTexts/gpt3_generated_text.txt", "a", encoding="utf-8") as fo:
         fo.write("\nGPT3 from " + prompt + ": " + response.encode('utf-8').decode('utf-8'))
     return response
